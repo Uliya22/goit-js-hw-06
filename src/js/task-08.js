f2c8dcf0c89a0efc57cb63b07fc12198b5653bd9
Uliya22 <uliya.d@ukr.net>
2022-08-07 22:22:49 +0300

    task 8

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,22 @@
+const form = document.querySelector('.login-form');
+let valuesForm = {};
+console.log(valuesForm);
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+    event.preventDefault();
+    const formEl = event.currentTarget.elements;
+    const mail = formEl.email.value;
+    const password = formEl.password.value;
+    valuesForm.email = mail;
+    valuesForm.password = password;
+
+    if (mail === '' || password === '') {
+        return alert('fill in all fields, please')
+    }
+    console.log(`Email: ${mail}, Password: ${password}`);
+        
+    form.reset();
+    };
+
 
